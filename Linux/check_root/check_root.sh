799074cad19941fb8862f76d15d901d8d6983f4c
nxdw <35192798+nxdw@users.noreply.github.com>
2019-09-18 15:42:43 +0800

    Update check_root.sh

--- Linux/check_root/check_root.sh
+++ Linux/check_root/check_root.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 PWD=`pwd`
-ip=192.168.1.6
 mail_self () {
         mail -s "$ip check_root" $1 < $PWD/check_root.txt
 }
@@ -11,7 +10,6 @@ check_root () {
         a=`df -Th /|awk -F' ' '{print $6}'|tail -n 1|cut -d % -f 1`
         if [ $a -ge 90 ];then
                 echo "[ERROR]:The root partition of $ip occupancy rate exceeds 90%!!!" > /root/check_root.txt
-mail_self dynxdw@163.com
         else
                 exit;
         fi
